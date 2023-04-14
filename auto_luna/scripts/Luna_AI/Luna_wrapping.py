898adb65f2f3c120db1cb5746355ac011958b1d9
Niallghus <74742450+BenjaminStimec@users.noreply.github.com>
2023-04-14 14:46:07 +0200

    INIT

---
+++ auto_luna/scripts/Luna_AI/Luna_wrapping.py
@@ -0,0 +1,3 @@
+#wraps misc LLM, wraps image generator, wraps speak, wraps other wrappers.
+#wrapper will need to give the AI access to the nest API, it manages control to tools
+#This program connects with the Django backend, both to use it as a bridge to the front and to have access to more delicate wrappers that may need to be separated
---
+++ auto_luna/scripts/llm_utils.py
@@ -0,0 +1,31 @@
+import openai
+from config import Config
+cfg = Config()
+
+openai.api_key = cfg.openai_api_key
+
+# Overly simple abstraction until we create something better
+def create_chat_completion(messages, model=None, temperature=None, max_tokens=None)->str:
+    if cfg.use_azure:
+        response = openai.ChatCompletion.create(
+            deployment_id=cfg.openai_deployment_id,
+            model=model,
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens
+        )
+    elif cfg.use_wrapper:
+        response = querywrapper(
+            message= messages,
+            path=cfg.wrapperpath
+        )
+    #Add elif for config option to use the wrapper, will query the respective wrapper providing path, messages, and temperature 
+    else:
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens
+        )
+
+    return response.choices[0].message["content"]
