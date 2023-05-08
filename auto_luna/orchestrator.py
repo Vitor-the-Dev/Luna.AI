6648507fd73c6bca8c49888300c4b8628d2c48e2
BenjaminStimec <74742450+BenjaminStimec@users.noreply.github.com>
2023-05-08 07:52:40 +0200

    orchestrator class fixes

--- auto_luna/orchestrator.py
+++ auto_luna/orchestrator.py
@@ -1,12 +1,13 @@
+import inspect
 import re
-
+import type_converter
 
 class orchestrator:
-    def __init__(self, llm_model, service_list):
-        self.llm_model = llm_model
-        self.service_list = service_list
-        self.function_bindings = {}
-        self.wrapperimport()
+    def __init__(self, llm_model, function_bindings):
+        self.model = llm_model
+        self.function_bindings = function_bindings
+        self.converter = type_converter()
+        self.model.get_reply("INITIAL PROMPT!")
 
     def validate_description_structure(description):
         if not isinstance(description, dict):
@@ -14,52 +15,40 @@ class orchestrator:
         required_keys = ["description", "function"]
         return all(key in description for key in required_keys)
 
-    def wrapperimport(self):
-        # import all wrappers parsed on config.json]
-        # binds the imported wrappers into a dict + description for different commands for the AI to use
-        # pass functions imported from wrappers as keys
+    def call_command(self, command_name, *arguments):
+        target_function = self.service_list[command_name]["function"]
+        function_signature = inspect.signature(target_function)
+        function_params = list(function_signature.parameters.values())
 
-        for obj in self.service_list:
-            all_command_names = set()
-            if hasattr(obj, "get_command_descriptions"):
-                service_name, commands = obj.get_command_descriptions()
+        if len(arguments) != len(function_params):
+            raise ValueError(f"Expected {len(function_params)} arguments, but got {len(arguments)}.")
 
-                if service_name in self.function_bindings:
-                    continue
+        converted_arguments = [
+            self.converter.convert_type(arg, param.annotation)
+            for arg, param in zip(arguments, function_params)
+        ]
 
-                for command_name, command_info in commands.items():
-                    if not self.validate_description_structure(command_info):
-                        continue
-
-                    if command_name in all_command_names:
-                        continue
-
-                    all_command_names.add(command_name)
-
-                self.function_bindings[service_name] = commands
-
-    def call_command(self, service_name, command_name, *arguments):
-        if command_name in self.function_bindings[service_name]:
-            self.service_list[service_name][command_name]["function"](
-                *arguments)
-            return True
-        else:
-            return False
+        target_function(*converted_arguments)
 
     def orchestrator_chat(self, prompt):
         # access the loaded llm set as orchestrator and creates an orchestrator instance, this should be used on the main function loop
         # # Print Assistant thoughts
         # print_assistant_thoughts(assistant_reply)
         reply = self.model.get_reply(prompt)
+
         command_pattern = re.compile(
-            r'\brun (\w+) (\w+)(.*)', flags=re.IGNORECASE)
+            r'\brun (\w+)\s*\((.*)\)', flags=re.IGNORECASE)
         matches = list(re.finditer(command_pattern, reply))
 
         responses = []
         for match in matches:
-            service_name = match.group(1)
-            command_name = match.group(2)
-            arguments = match.group(3).strip().split()
-            self.call_command(service_name, command_name, *arguments)
+            command_name = match.group(1)
+            arguments_str = match.group(2).strip()
+            if arguments_str:
+                arguments = arguments_str.split(', ')
+            else:
+                arguments = []
 
-        return (reply)
+            self.call_command(command_name, *arguments)
+
+        return reply
